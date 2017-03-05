a81d9e7bac21a57c2b2fb577bf351238520a0984
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-03-05 13:06:21 +0200

    startProject

---
+++ LoginTest/LoginTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginTest
+//
+//  Created by Stanly Shiyanovskiy on 05.03.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
